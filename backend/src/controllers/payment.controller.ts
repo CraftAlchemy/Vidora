e1d2aef1e6c7282a883e3b4fbf2ebbc3a9241aae
apachitech <liandjadavid@gmail.com>
2025-09-20 16:01:00 +0200

    Stripe Downgrade

--- backend/src/controllers/payment.controller.ts
+++ backend/src/controllers/payment.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
 });
 
 export const createPaymentIntent = async (req: Request, res: Response) => {
